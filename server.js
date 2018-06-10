1791caf0014c1d3d6881b339e92ff9aa9f11e098
ngdevelop-tech <aprajapati06@gmail.com>
2018-06-10 10:29:30 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs')
+
+//express middleware
+app.use((req, res, next) => {
+
+    var logTime = new Date().toString();
+    fs.appendFile('server.log', `${logTime} : ${req.method} ${req.url}\n`, (error) => {
+        
+    }
+    );
+    next();
+});
+
+
+// app.use( (req, res, next) => {
+//     res.render('maintenance.hbs');
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'NgDevelop',
+        welcomeMsg: 'Welcome to Node Practice'
+    })
+})
+
+app.get('/api', (req, res) => {
+    res.send({ 'userName': 'Ankit', 'role': 'developer' });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Us',
+        features: 'Great Tutorials'
+    })
+})
+
+app.listen('3000', () => {
+    console.log('Server is up on port 3000');
+
+});
